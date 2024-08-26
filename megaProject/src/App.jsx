f158f92205dec8664e2a7b4490aebe89f674aa94
ayyarsandesh <ayyarsandesh@gmail.com>
2024-08-26 22:59:45 +0530

    removed uneccessary import

---
+++ megaProject/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import {useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import './App.css'
+import {Footer ,Header} from './components/index.js'
+import {Outlet} from 'react-router-dom'
+
+function App() {
+ const [loading,setLoading]=useState(true);
+ const dispatch=useDispatch();
+
+ useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+ },[])
+
+  return !loading ?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+       TODO: {/* <Outlet /> */}
+        <Footer />
+      </div>
+    </div>
+  ) :null
+}
+
+export default App
--- megaProject/src/main.jsx
+++ megaProject/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import {store} from './app/store.js'
+import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
+  <StrictMode>
+    <Provider store={store} >
       <App />
-    </Provider>,
+    </Provider>
+  </StrictMode>,
 )
